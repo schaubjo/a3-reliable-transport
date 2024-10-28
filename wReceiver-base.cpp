f72ba3afddb4e66f04a33dfa45e6678cb3bb3a18
schaubjo <93454118+schaubjo@users.noreply.github.com>
2024-10-27 23:40:18 -0400

    Remove comments

--- wReceiver-base.cpp
+++ wReceiver-base.cpp
@@ -7,8 +7,6 @@
 #include <thread>
 #include <unistd.h>
 
-const int BUFFER_SIZE = 1024;
-
 #define MAX_PACKET_SIZE 1472
 #define START 0
 #define END 1
@@ -27,7 +25,6 @@ int main(int argc, char *argv[]) {
 
   int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
   struct sockaddr_in server_addr, client_addr;
-  char buffer[BUFFER_SIZE];
 
   // Define server address
   memset(&server_addr, 0, sizeof(server_addr));
@@ -86,80 +83,8 @@ int main(int argc, char *argv[]) {
       std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
   }
-  // // Receive messages
-  // ssize_t received_bytes = recvfrom(sockfd, buffer, BUFFER_SIZE, 0,
-  //                                   (struct sockaddr *)&client_addr,
-  //                                   &addr_len);
-
-  // if (received_bytes < 0) {
-  //   perror("Receive failed");
-  //   close(sockfd);
-  //   return 1;
-  // }
-
-  // buffer[received_bytes] = '\0'; // Null-terminate the received string
-  // std::cout << "Message received: " << buffer << std::endl;
-
-  // // Send ACK message back to the sender
-  // const char *ack_message = "ACK: Message received";
-  // ssize_t sent_bytes = sendto(sockfd, ack_message, strlen(ack_message), 0,
-  //                             (const struct sockaddr *)&client_addr,
-  //                             addr_len);
-
-  // if (sent_bytes < 0) {
-  //   perror("ACK send failed");
-  //   close(sockfd);
-  //   return 1;
-  // }
-
-  // std::cout << "ACK sent: " << ack_message << std::endl;
 
   // Close the socket
   close(sockfd);
   return 0;
 }
-
-// using namespace std;
-
-// int main(int argc, char *argv[]) {
-//   if (argc != 5) {
-//     printf("Usage: %s <port-num> <window-size> <output-dir> <log>\n",
-//     argv[0]); return 1;
-//   }
-//   int port_num = stoi(argv[1]);
-//   int window_size = stoi(argv[2]);
-//   string output_dir = argv[3];
-//   string log = argv[4];
-
-//   ofstream log_file(log, ofstream::app);
-//   log_file.close();
-
-// // Create UDP socket
-// int sockfd = make_udp_socket();
-// if (sockfd < 0) {
-//   cerr << "Failed to create receiver UDP socket." << endl;
-// }
-
-// // Set the "reuse port" socket option
-// int yes = 1;
-// setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-
-// // Create sockaddr_in for port and bind to it
-// struct sockaddr_in addr;
-// if (make_server_sockaddr(&addr, port_num) == -1) {
-//   cerr << "Failed to make server sockaddr" << endl;
-//   return -1;
-// }
-
-// bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
-
-// // Receive a packet
-// PacketHeader header;
-// while (true) {
-//   if (receive_packet(sockfd, header, addr) < 0) {
-//     return -1;
-//   }
-//   cout << "Successfully received packet" << endl;
-//   cout << "Checksum: " << header.checksum << endl;
-// }
-// }
--- wSender-base.cpp
+++ wSender-base.cpp
@@ -35,8 +35,6 @@ int main(int argc, char *argv[]) {
   }
 
   struct sockaddr_in server_addr;
-  const char *message = "Hello, new UDP Receiver!";
-  char ack_buffer[1024];
   socklen_t addr_len = sizeof(server_addr);
 
   // Define server address
@@ -89,101 +87,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  // if (ntohl(ack_header.type) == ACK &&
-  //     ntohl(ack_header.seqNum) == htonl(start_packet.seqNum)) {
-  //   std::cout << "ACK for START received" << std::endl;
-  // }
-  // // Send message to the receiver
-  // ssize_t sent_bytes =
-  //     sendto(sockfd, message, strlen(message), 0,
-  //            (const struct sockaddr *)&server_addr, sizeof(server_addr));
-
-  // if (sent_bytes < 0) {
-  //   perror("Send failed");
-  //   close(sockfd);
-  //   return 1;
-  // }
-
-  // std::cout << "Message sent: " << message << std::endl;
-
-  // // Wait for ACK from the receiver
-  // ssize_t ack_received = recvfrom(sockfd, ack_buffer, sizeof(ack_buffer) - 1,
-  // 0,
-  //                                 (struct sockaddr *)&server_addr,
-  //                                 &addr_len);
-
-  // if (ack_received < 0) {
-  //   perror("ACK receive failed");
-  //   close(sockfd);
-  //   return 1;
-  // }
-
-  // ack_buffer[ack_received] = '\0'; // Null-terminate the ACK string
-  // std::cout << "ACK received: " << ack_buffer << std::endl;
-
   // Close the socket
   close(sockfd);
   return 0;
 }
-
-// #define MAX_PACKET_SIZE 1472
-// #define START 0
-// #define END 1
-// #define DATA 2
-// #define ACK 3
-
-// using namespace std;
-
-// int main(int argc, char *argv[]) {
-// if (argc != 6) {
-//   printf("Usage: %s <receiver_ip> <receiver_port> <window_size> "
-//          "<input_filename> <log_filename>\n",
-//          argv[0]);
-//   return 1;
-// }
-
-// string receiver_ip = argv[1];
-// int receiver_port = stoi(argv[2]);
-// int window_size = stoi(argv[3]);
-// string input_filename = argv[4];
-// string log_filename = argv[5];
-
-//   // // ofstream log_file(log_filename, ofstream::app);
-//   // // log_file.close();
-
-//   // // ifstream input_file(input_filename);
-
-//   // // if (!input_file.is_open()) {
-//   // //   printf("Error: Cannot open input file\n");
-//   // //   return 1;
-//   // // }
-
-//   // // Create UDP socket
-//   // int client_sock = make_udp_socket();
-//   // struct sockaddr_in addr;
-//   // const char *hostname = receiver_ip.c_str();
-//   // if (make_client_sockaddr(&addr, hostname, receiver_port) == -1) {
-//   //   cerr << "Error making client sockaddr" << endl;
-//   // }
-
-//   // // Send START message to initiate connection
-//   // // START: type = 0; seqNum = random(); length = 0; checksum = ?
-//   // PacketHeader header;
-//   // header.type = htonl(0);
-//   // header.seqNum = htonl(1);   // TODO: make rand()
-//   // header.checksum = htonl(2); // TODO: change
-//   // header.length = htonl(0);   // Set to 0 for START
-//   // const char *data = "";
-//   // if (send_packet(client_sock, header, data, addr) < 0) {
-//   //   return 1;
-//   // }
-//   // std::this_thread::sleep_for(std::chrono::seconds(100));
-//   // // Wait for ACK of START
-
-//   // // Do main logic of sending file data
-
-//   // // Send END message to end connection
-//   // // END: type = 1; seqNum = same as START; length = 0; checksum = ?
-
-//   // // Wait for ACK of END
-// }
